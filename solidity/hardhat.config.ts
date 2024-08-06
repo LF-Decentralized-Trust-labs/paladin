a0344952ad39581e80f4db83386ef5a5fbf1bba8
Andrew Richardson <andrew.richardson@kaleido.io>
2024-08-06 16:09:19 -0400

    Pull in Zeto contracts to Solidity build
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -1,11 +1,12 @@
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "@typechain/hardhat";
+import "hardhat-dependency-compiler";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.24",
+    version: "0.8.20",
     settings: {
       evmVersion: "paris",
       optimizer: {
@@ -13,6 +14,16 @@ const config: HardhatUserConfig = {
         runs: 1000,
       },
     },
+  },
+  dependencyCompiler: {
+    paths: [
+      "zeto/contracts/zeto_anon.sol",
+      "zeto/contracts/zeto_anon_enc.sol",
+      "zeto/contracts/zeto_anon_enc_nullifier.sol",
+      "zeto/contracts/zeto_anon_nullifier.sol",
+      "zeto/contracts/zeto_nf_anon.sol",
+      "zeto/contracts/zeto_nf_anon_nullifier.sol",
+    ],
   },
 };
 
