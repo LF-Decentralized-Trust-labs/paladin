66736c0e91c7aad6b6286f85f187190bc0e944cc
Anna McAllister <anna.mcallister@kaleido.io>
2025-05-14 12:24:02 +0100

    add remaining ff resty config fields
    Signed-off-by: Anna McAllister <anna.mcallister@kaleido.io>

--- config/pkg/pldconf/httpclient.go
+++ config/pkg/pldconf/httpclient.go
@@ -25,16 +25,51 @@ type HTTPBasicAuthConfig struct {
 	Password string `json:"password"`
 }
 
+type HTTPProxyConfig struct {
+	URL string `json:"url"`
+}
+
+type HTTPRetryConfig struct {
+	Count                *int    `json:"count,omitempty"`
+	Enabled              bool    `json:"enabled,omitempty"`
+	ErrorStatusCodeRegex string  `json:"errorStatusCodeRegex,omitempty"`
+	InitialDelay         *string `json:"initialDelay,omitempty"`
+	MaximumDelay         *string `json:"maxDelay,omitempty"`
+}
+
+type HTTPThrottleConfig struct {
+	Burst             *int `json:"burst,omitempty"`
+	RequestsPerSecond *int `json:"requestsPerSecond,omitempty"`
+}
+
 type HTTPClientConfig struct {
-	URL               string                 `json:"url"`
-	HTTPHeaders       map[string]interface{} `json:"httpHeaders"`
-	Auth              HTTPBasicAuthConfig    `json:"auth"`
-	TLS               TLSConfig              `json:"tls"`
-	RequestTimeout    *string                `json:"requestTimeout,omitempty"`
-	ConnectionTimeout *string                `json:"connectionTimeout,omitempty"`
+	URL                   string                 `json:"url"`
+	HTTPHeaders           map[string]interface{} `json:"httpHeaders"`
+	Auth                  HTTPBasicAuthConfig    `json:"auth"`
+	TLS                   TLSConfig              `json:"tls"`
+	Proxy                 HTTPProxyConfig        `json:"proxy"`
+	Retry                 HTTPRetryConfig        `json:"retry"`
+	Throttle              HTTPThrottleConfig     `json:"throttle"`
+	RequestTimeout        *string                `json:"requestTimeout,omitempty"`
+	ConnectionTimeout     *string                `json:"connectionTimeout,omitempty"`
+	ExpectContinueTimeout *string                `json:"expectContinueTimeout,omitempty"`
+	IdleConnTimeout       *string                `json:"idleConnTimeout,omitempty"`
+	MaxConnsPerHost       *int                   `json:"maxConnsPerHost,omitempty"`
+	MaxIdleConns          *int                   `json:"maxIdleConns,omitempty"`
+	TLSHandshakeTimeout   *string                `json:"tlsHandshakeTimeout,omitempty"`
 }
 
 var DefaultHTTPConfig = &HTTPClientConfig{
-	ConnectionTimeout: confutil.P("30s"),
-	RequestTimeout:    confutil.P("30s"),
+	ConnectionTimeout:     confutil.P("30s"),
+	RequestTimeout:        confutil.P("30s"),
+	ExpectContinueTimeout: confutil.P("1s"),
+	IdleConnTimeout:       confutil.P("475ms"),
+	MaxConnsPerHost:       confutil.P(0),
+	MaxIdleConns:          confutil.P(100),
+	TLSHandshakeTimeout:   confutil.P("10s"),
+	Retry: HTTPRetryConfig{
+		Count:        confutil.P(5),
+		InitialDelay: confutil.P("250s"),
+		MaximumDelay: confutil.P("30s"),
+	},
 }
